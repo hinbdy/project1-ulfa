76ad242211ed8c2e725635a96de36590f3370ed5
hinbdy <ufhdynt@gmail.com>
2024-07-10 00:31:13 +0700

    Project Pengganti UTS

---
+++ app/Config/MySite.php
@@ -0,0 +1,8 @@
+<?php
+namespace Config;
+use CodeIgniter\Config\BaseConfig;
+class MySite extends BaseConfig
+{
+public $siteName = "Hello ulfaaa";
+public $siteEmail = "ulfhdynt@gmail.com";
+}
---
+++ app/Views/partials/navbar.php
@@ -0,0 +1,4 @@
+<ul style="list-style-type: none;">
+    <li style="display: inline;padding: 5px;color:blue;">Home</li>
+    <li style="display: inline;padding: 5px;color:blue;">About</li>
+</ul>
